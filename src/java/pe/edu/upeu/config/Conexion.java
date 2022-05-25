13dad200cf1d64c245f601382a9c42b79d9799db
Digi85 <73599904+Digi85@users.noreply.github.com>
2022-05-25 12:58:48 -0500

    first commit

---
+++ src/java/pe/edu/upeu/config/Conexion.java
@@ -0,0 +1,30 @@
+package pe.edu.upeu.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author NITRO5
+ */
+public class Conexion {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/examen_parcial";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String USER = "root";
+    private static final String PASS = "12345";
+    
+    private static Connection cx = null;
+
+    public static Connection getConex() {
+        try {
+            Class.forName(DRIVER);
+            if (cx == null) {
+                cx = DriverManager.getConnection(URL, USER, PASS);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error:" + e);
+        }
+        return cx;
+    }
+}
